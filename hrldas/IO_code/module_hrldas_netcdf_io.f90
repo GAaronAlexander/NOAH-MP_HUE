e3e54fe16c33ba00f52536bce59257444dc2f4e8
Gary Alexander <galexand@cheyenne3.cheyenne.ucar.edu>
2022-06-23 16:03:34 -0600

    makefile changed

--- hrldas/IO_code/module_hrldas_netcdf_io.f90
+++ hrldas/IO_code/module_hrldas_netcdf_io.f90
@@ -2412,11 +2412,6 @@ contains
       integer nf_float
       integer nf_real
       integer nf_double
-      integer nf_ubyte
-      integer nf_ushort
-      integer nf_uint
-      integer nf_int64
-      integer nf_uint64
 
       parameter (nf_byte = 1)
       parameter (nf_int1 = nf_byte)
@@ -2427,11 +2422,6 @@ contains
       parameter (nf_float = 5)
       parameter (nf_real = nf_float)
       parameter (nf_double = 6)
-      parameter (nf_ubyte = 7)
-      parameter (nf_ushort = 8)
-      parameter (nf_uint = 9)
-      parameter (nf_int64 = 10)
-      parameter (nf_uint64 = 11)
 
 !
 ! default fill values:
@@ -2468,15 +2458,10 @@ contains
       integer nf_lock
       integer nf_share
       integer nf_64bit_offset
-      integer nf_64bit_data
-      integer nf_cdf5
       integer nf_sizehint_default
       integer nf_align_chunk
       integer nf_format_classic
       integer nf_format_64bit
-      integer nf_format_64bit_offset
-      integer nf_format_64bit_data
-      integer nf_format_cdf5
       integer nf_diskless
       integer nf_mmap
 
@@ -2489,15 +2474,10 @@ contains
       parameter (nf_lock = 1024)
       parameter (nf_share = 2048)
       parameter (nf_64bit_offset = 512)
-      parameter (nf_64bit_data = 32)
-      parameter (nf_cdf5 = nf_64bit_data)
       parameter (nf_sizehint_default = 0)
       parameter (nf_align_chunk = -1)
       parameter (nf_format_classic = 1)
       parameter (nf_format_64bit = 2)
-      parameter (nf_format_64bit_offset = nf_format_64bit)
-      parameter (nf_format_64bit_data = 5)
-      parameter (nf_format_cdf5 = nf_format_64bit_data)
       parameter (nf_diskless = 8)
       parameter (nf_mmap = 16)
 
@@ -2930,22 +2910,6 @@ contains
 !                          character(*)        name,
 !                          integer             ivals(1))
       external        nf_get_att_int
-
-      integer         nf_put_att_int64
-!                         (integer             ncid,
-!                          integer             varid,
-!                          character(*)        name,
-!                          integer             xtype,
-!                          integer             len,
-!                          nf_int8_t           i8vals(1))
-      external        nf_put_att_int64
-
-      integer         nf_get_att_int64
-!                         (integer             ncid,
-!                          integer             varid,
-!                          character(*)        name,
-!                          nf_int8_t           i8vals(1))
-      external        nf_get_att_int64
 
       integer         nf_put_att_real
 !                         (integer             ncid,
@@ -3550,28 +3514,6 @@ contains
 !                          doubleprecision     dvals(1))
       external        nf_get_varm_double
 
-!     64-bit int functions.
-      integer nf_put_var1_int64
-      external nf_put_var1_int64
-      integer nf_put_vara_int64
-      external nf_put_vara_int64
-      integer nf_put_vars_int64
-      external nf_put_vars_int64
-      integer nf_put_varm_int64
-      external nf_put_varm_int64
-      integer nf_put_var_int64
-      external nf_put_var_int64
-      integer nf_get_var1_int64
-      external nf_get_var1_int64
-      integer nf_get_vara_int64
-      external nf_get_vara_int64
-      integer nf_get_vars_int64
-      external nf_get_vars_int64
-      integer nf_get_varm_int64
-      external nf_get_varm_int64
-      integer nf_get_var_int64
-      external nf_get_var_int64
-
 
 !     NetCDF-4.
 !     This is part of netCDF-4. Copyright 2006, UCAR, See COPYRIGHT
@@ -3582,12 +3524,22 @@ contains
 !     $Id: netcdf4.inc,v 1.28 2010/05/25 13:53:02 ed Exp $
 
 !     New netCDF-4 types.
+      integer nf_ubyte
+      integer nf_ushort
+      integer nf_uint
+      integer nf_int64
+      integer nf_uint64
       integer nf_string
       integer nf_vlen
       integer nf_opaque
       integer nf_enum
       integer nf_compound
 
+      parameter (nf_ubyte = 7)
+      parameter (nf_ushort = 8)
+      parameter (nf_uint = 9)
+      parameter (nf_int64 = 10)
+      parameter (nf_uint64 = 11)
       parameter (nf_string = 12)
       parameter (nf_vlen = 13)
       parameter (nf_opaque = 14)
@@ -3635,8 +3587,6 @@ contains
       parameter (nf_chunked = 0)
       integer nf_contiguous
       parameter (nf_contiguous = 1)
-      integer nf_compact
-      parameter (nf_compact = 2)
 
 !     For NF_DEF_VAR_FLETCHER32
       integer nf_nochecksum
@@ -3781,12 +3731,6 @@ contains
       integer nf_inq_var_deflate
       external nf_inq_var_deflate
 
-      integer nf_def_var_szip
-      external nf_def_var_szip
-
-      integer nf_inq_var_szip
-      external nf_inq_var_szip
-
       integer nf_def_var_fletcher32
       external nf_def_var_fletcher32
 
@@ -3810,12 +3754,6 @@ contains
 
       integer nf_inq_var_endian
       external nf_inq_var_endian
-
-      integer nf_def_var_filter
-      external nf_def_var_filter
-
-      integer nf_inq_var_filter
-      external nf_inq_var_filter
 
 !     User defined types.
       integer nf_inq_typeids
@@ -3931,6 +3869,28 @@ contains
       external nf_get_vara
       integer nf_get_vars
       external nf_get_vars
+
+!     64-bit int functions.
+      integer nf_put_var1_int64
+      external nf_put_var1_int64
+      integer nf_put_vara_int64
+      external nf_put_vara_int64
+      integer nf_put_vars_int64
+      external nf_put_vars_int64
+      integer nf_put_varm_int64
+      external nf_put_varm_int64
+      integer nf_put_var_int64
+      external nf_put_var_int64
+      integer nf_get_var1_int64
+      external nf_get_var1_int64
+      integer nf_get_vara_int64
+      external nf_get_vara_int64
+      integer nf_get_vars_int64
+      external nf_get_vars_int64
+      integer nf_get_varm_int64
+      external nf_get_varm_int64
+      integer nf_get_var_int64
+      external nf_get_var_int64
 
 !     For helping F77 users with VLENs.
       integer nf_get_vlen_element
@@ -4175,10 +4135,6 @@ contains
       parameter (fillong = -2147483647)
       parameter (filfloat = 9.9692099683868690e+36)
       parameter (fildoub = 9.9692099683868690e+36)
-
-!     This is to turn on netCDF internal logging.
-      integer nf_set_log_level
-      external nf_set_log_level
 
     character(len=*),                         intent(in) :: outdir
     character(len=*),                         intent(in) :: version
@@ -4851,11 +4807,6 @@ contains
       integer nf_float
       integer nf_real
       integer nf_double
-      integer nf_ubyte
-      integer nf_ushort
-      integer nf_uint
-      integer nf_int64
-      integer nf_uint64
 
       parameter (nf_byte = 1)
       parameter (nf_int1 = nf_byte)
@@ -4866,11 +4817,6 @@ contains
       parameter (nf_float = 5)
       parameter (nf_real = nf_float)
       parameter (nf_double = 6)
-      parameter (nf_ubyte = 7)
-      parameter (nf_ushort = 8)
-      parameter (nf_uint = 9)
-      parameter (nf_int64 = 10)
-      parameter (nf_uint64 = 11)
 
 !
 ! default fill values:
@@ -4907,15 +4853,10 @@ contains
       integer nf_lock
       integer nf_share
       integer nf_64bit_offset
-      integer nf_64bit_data
-      integer nf_cdf5
       integer nf_sizehint_default
       integer nf_align_chunk
       integer nf_format_classic
       integer nf_format_64bit
-      integer nf_format_64bit_offset
-      integer nf_format_64bit_data
-      integer nf_format_cdf5
       integer nf_diskless
       integer nf_mmap
 
@@ -4928,15 +4869,10 @@ contains
       parameter (nf_lock = 1024)
       parameter (nf_share = 2048)
       parameter (nf_64bit_offset = 512)
-      parameter (nf_64bit_data = 32)
-      parameter (nf_cdf5 = nf_64bit_data)
       parameter (nf_sizehint_default = 0)
       parameter (nf_align_chunk = -1)
       parameter (nf_format_classic = 1)
       parameter (nf_format_64bit = 2)
-      parameter (nf_format_64bit_offset = nf_format_64bit)
-      parameter (nf_format_64bit_data = 5)
-      parameter (nf_format_cdf5 = nf_format_64bit_data)
       parameter (nf_diskless = 8)
       parameter (nf_mmap = 16)
 
@@ -5369,22 +5305,6 @@ contains
 !                          character(*)        name,
 !                          integer             ivals(1))
       external        nf_get_att_int
-
-      integer         nf_put_att_int64
-!                         (integer             ncid,
-!                          integer             varid,
-!                          character(*)        name,
-!                          integer             xtype,
-!                          integer             len,
-!                          nf_int8_t           i8vals(1))
-      external        nf_put_att_int64
-
-      integer         nf_get_att_int64
-!                         (integer             ncid,
-!                          integer             varid,
-!                          character(*)        name,
-!                          nf_int8_t           i8vals(1))
-      external        nf_get_att_int64
 
       integer         nf_put_att_real
 !                         (integer             ncid,
@@ -5989,28 +5909,6 @@ contains
 !                          doubleprecision     dvals(1))
       external        nf_get_varm_double
 
-!     64-bit int functions.
-      integer nf_put_var1_int64
-      external nf_put_var1_int64
-      integer nf_put_vara_int64
-      external nf_put_vara_int64
-      integer nf_put_vars_int64
-      external nf_put_vars_int64
-      integer nf_put_varm_int64
-      external nf_put_varm_int64
-      integer nf_put_var_int64
-      external nf_put_var_int64
-      integer nf_get_var1_int64
-      external nf_get_var1_int64
-      integer nf_get_vara_int64
-      external nf_get_vara_int64
-      integer nf_get_vars_int64
-      external nf_get_vars_int64
-      integer nf_get_varm_int64
-      external nf_get_varm_int64
-      integer nf_get_var_int64
-      external nf_get_var_int64
-
 
 !     NetCDF-4.
 !     This is part of netCDF-4. Copyright 2006, UCAR, See COPYRIGHT
@@ -6021,12 +5919,22 @@ contains
 !     $Id: netcdf4.inc,v 1.28 2010/05/25 13:53:02 ed Exp $
 
 !     New netCDF-4 types.
+      integer nf_ubyte
+      integer nf_ushort
+      integer nf_uint
+      integer nf_int64
+      integer nf_uint64
       integer nf_string
       integer nf_vlen
       integer nf_opaque
       integer nf_enum
       integer nf_compound
 
+      parameter (nf_ubyte = 7)
+      parameter (nf_ushort = 8)
+      parameter (nf_uint = 9)
+      parameter (nf_int64 = 10)
+      parameter (nf_uint64 = 11)
       parameter (nf_string = 12)
       parameter (nf_vlen = 13)
       parameter (nf_opaque = 14)
@@ -6074,8 +5982,6 @@ contains
       parameter (nf_chunked = 0)
       integer nf_contiguous
       parameter (nf_contiguous = 1)
-      integer nf_compact
-      parameter (nf_compact = 2)
 
 !     For NF_DEF_VAR_FLETCHER32
       integer nf_nochecksum
@@ -6220,12 +6126,6 @@ contains
       integer nf_inq_var_deflate
       external nf_inq_var_deflate
 
-      integer nf_def_var_szip
-      external nf_def_var_szip
-
-      integer nf_inq_var_szip
-      external nf_inq_var_szip
-
       integer nf_def_var_fletcher32
       external nf_def_var_fletcher32
 
@@ -6249,12 +6149,6 @@ contains
 
       integer nf_inq_var_endian
       external nf_inq_var_endian
-
-      integer nf_def_var_filter
-      external nf_def_var_filter
-
-      integer nf_inq_var_filter
-      external nf_inq_var_filter
 
 !     User defined types.
       integer nf_inq_typeids
@@ -6370,6 +6264,28 @@ contains
       external nf_get_vara
       integer nf_get_vars
       external nf_get_vars
+
+!     64-bit int functions.
+      integer nf_put_var1_int64
+      external nf_put_var1_int64
+      integer nf_put_vara_int64
+      external nf_put_vara_int64
+      integer nf_put_vars_int64
+      external nf_put_vars_int64
+      integer nf_put_varm_int64
+      external nf_put_varm_int64
+      integer nf_put_var_int64
+      external nf_put_var_int64
+      integer nf_get_var1_int64
+      external nf_get_var1_int64
+      integer nf_get_vara_int64
+      external nf_get_vara_int64
+      integer nf_get_vars_int64
+      external nf_get_vars_int64
+      integer nf_get_varm_int64
+      external nf_get_varm_int64
+      integer nf_get_var_int64
+      external nf_get_var_int64
 
 !     For helping F77 users with VLENs.
       integer nf_get_vlen_element
@@ -6614,10 +6530,6 @@ contains
       parameter (fillong = -2147483647)
       parameter (filfloat = 9.9692099683868690e+36)
       parameter (fildoub = 9.9692099683868690e+36)
-
-!     This is to turn on netCDF internal logging.
-      integer nf_set_log_level
-      external nf_set_log_level
 
     character(len=*),                      intent(in) :: outdir
     character(len=*),                      intent(in) :: version
